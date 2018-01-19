f3962b367aa9335837859162a9eaa1c720509b6b
torreswoo <torreswoo@gmail.com>
2018-01-19 13:50:23 +0900

    test | add repository test - UserActionLogRepository

---
+++ src/test/java/com/kakaopay/AppMainTest.java
@@ -0,0 +1,23 @@
+package com.kakaopay;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class AppMainTest {
+
+    @Before
+    public void setup() throws Exception {
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
